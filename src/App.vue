629a04837041cb8fbec63495deee08237b8fe27b
RaixsG <jhardelloficial@gmail.com>
2024-01-17 18:59:47 -0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<script setup>
+import TheWelcome from './components/TheWelcome.vue'
+</script>
+
+<template>
+  <nav class="nav">
+    <a href="">Clientes</a>
+    <span>|</span>
+    <a href="">Clientes Frecuentes</a>
+    <span>|</span>
+    <a href="">Empleados</a>
+    <span>|</span>
+    <a href="">Productos</a>
+    <span>|</span>
+    <a href="">Tipo de Productos</a>
+  </nav>
+  <main>
+    <TheWelcome />
+  </main>
+</template>
+
+<style scoped>
+  .nav {
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    background-color: #333;
+    color: #fff;
+    padding: 1rem;
+  }
+</style>
